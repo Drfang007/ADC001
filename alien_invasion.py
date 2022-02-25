54ae325678f0b116813a57924622e85ba27051ef
fang007 <fang.japan.hirosakiuniversity@gmail.com>
2022-02-25 16:34:46 +0900

    添加图像前准备 2月25日保存

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,18 +1,20 @@
 import sys
 import pygame
+from settings import Settings
 
 class AlienInvasion:
 	"""管理游戏资源和行为的类"""
 	def __init__(self):
 		"""初始化游戏并创建资源"""
 		pygame.init()
-
-		self.screen = pygame.display.set_mode((1200,800))
+		self.settings = Settings()
+		self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
 		pygame.display.set_caption("Alien Invasion")
-		self.bg_color = (230, 230, 230)
+		
 
 	def run_game(self):
 		"""开始游戏主循环"""
+		self.bg_color = (self.settings.bg_color)
 		while True:
 			# 监视键盘和鼠标事件。
 			for event in pygame.event.get():
