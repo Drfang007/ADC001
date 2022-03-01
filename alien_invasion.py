20c3a6825658fb93e8c6475a549e7f35967efd85
fang007 <fang.japan.hirosakiuniversity@gmail.com>
2022-03-01 10:52:02 +0900

    飞船准备好了，起飞！

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
 	"""管理游戏资源和行为的类"""
@@ -13,6 +14,7 @@ class AlienInvasion:
 		pygame.display.set_caption("Alien Invasion")
 		
 		self.ship =Ship(self)
+		self.bullets = pygame.sprite.Group()
 
 		
 
@@ -39,6 +41,8 @@ class AlienInvasion:
 			self.ship.moving_left = True
 		elif event.key == pygame.K_q:
 			sys.exit()
+		elif event.key == pygame.K_SPACE:
+			self._fire_bullet()
 
 	def _check_keyup_events(self,event):
 		"""按键松开"""
@@ -49,10 +53,26 @@ class AlienInvasion:
 			#取消向左移动飞船
 			self.ship.moving_left = False
 
+	def _fire_bullet(self):
+		"""创建一颗子弹并发射出去"""
+		if len(self.bullets)< self.settings.bullets_allowed:
+			new_bullet = Bullet(self)
+			self.bullets.add(new_bullet)
+	def _update_bullets(self):
+		self.bullets.update()
+		#删除消失的子弹
+		for bullet in self.bullets.copy():
+			if bullet.rect.bottom <= 0:
+				self.bullets.remove(bullet)
+				print(len(self.bullets))			
+
 	def _update_screen(self):
 		self.bg_color = (self.settings.bg_color)
 		self.screen.fill(self.bg_color)
 		self.ship.blitme()
+		for bullet in self.bullets.sprites():
+			bullet.draw_bullet()
+
 		"""让最近绘制的屏幕可见"""
 		pygame.display.flip()
 
@@ -62,7 +82,8 @@ class AlienInvasion:
 			self._check_enents()
 			self._update_screen()
 			self.ship.update()
-				
+			self._update_bullets()
+			
 
 if __name__ == '__main__':
 	#创建游戏实例并运行游戏。
